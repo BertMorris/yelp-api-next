f67f02d3d9a3145f225124a94b7a99db27d6126f
BertMorris <bertmorris911@gmail.com>
2023-05-04 08:40:00 -0700

    fix spelling

--- src/pages/[location]/index.tsx
+++ src/pages/[location]/index.tsx
@@ -5,13 +5,11 @@ import { useRouter } from "next/router";
 type Props = {};
 
 export default function Cuisines({}: Props) {
-
   const [left, setLeft] = useState<string[]>(["", "", ""]);
   const [right, setRight] = useState<string[]>(["", "", ""]);
-  const [location, setLocation] = useState('');
+  const [location, setLocation] = useState("");
   const [selectedRadius, setSelectedRadius] = useState(1);
   const [selectedOptions, setSelectedOptions] = useState<number[]>([]);
-
 
   const router = useRouter();
   const cuisines = [
@@ -21,39 +19,35 @@ export default function Cuisines({}: Props) {
     "Chinese",
     "Mexican",
     "Thai",
-    "Vegitarian",
+    "Vegetarian",
     "Mediterranean",
     "Indian",
   ];
 
-
-  const handleOptionClick = (option:any) => {
+  const handleOptionClick = (option: any) => {
     if (selectedOptions.includes(option)) {
-      setSelectedOptions(selectedOptions.filter((selectedOption) => selectedOption !== option));
+      setSelectedOptions(
+        selectedOptions.filter((selectedOption) => selectedOption !== option)
+      );
     } else {
       setSelectedOptions([...selectedOptions, option]);
     }
 
     console.log(selectedOptions);
-    
 
     // console.log(containsFour);
   };
-  
 
-
-
-
-  const handleLocationChange = (event:any) => {
+  const handleLocationChange = (event: any) => {
     setLocation(event.target.value);
     console.log(event.target.value);
   };
 
-  const handleRadiusChange = (event:any) => {
+  const handleRadiusChange = (event: any) => {
     setSelectedRadius(parseInt(event.target.value));
     console.log(event.target.value);
   };
-  
+
   function isDisabled() {
     if (left.includes("") || right.includes("")) {
       return true;
@@ -89,34 +83,71 @@ export default function Cuisines({}: Props) {
         Get Restaurants
       </button>
 
+      <form>
+        <label htmlFor="radius">Select Radius:</label>
+        <select
+          id="radius"
+          name="radius"
+          value={selectedRadius}
+          onChange={handleRadiusChange}
+        >
+          <option value={1}>Less than 1 mile</option>
+          <option value={10}>Less than 10 miles</option>
+          <option value={25}>Less than 25 miles</option>
+          <option value={50}>Less than 50 miles</option>
+          <option value={100}>Less than 100 miles</option>
+        </select>
+      </form>
 
       <form>
-      <label htmlFor="radius">Select Radius:</label>
-      <select id="radius" name="radius" value={selectedRadius} onChange={handleRadiusChange}>
-        <option value={1}>Less than 1 mile</option>
-        <option value={10}>Less than 10 miles</option>
-        <option value={25}>Less than 25 miles</option>
-        <option value={50}>Less than 50 miles</option>
-        <option value={100}>Less than 100 miles</option>
-      </select>
-    </form>
+        <label htmlFor="location">Enter Location:</label>
+        <input
+          type="text"
+          id="location"
+          name="location"
+          placeholder="e.g. New York City"
+          value={location}
+          onChange={handleLocationChange}
+        />
+      </form>
 
-      <form>
-      <label htmlFor="location">Enter Location:</label>
-      <input type="text" id="location" name="location" placeholder="e.g. New York City" value={location} onChange={handleLocationChange} />
-    </form>
-
-
-    <div className="App">
-      <h1>Select Options:</h1>
-      <div className="button-group">
-        <button className={`button ${selectedOptions.includes(1) ? "selected" : ""}`} onClick={() => handleOptionClick(1)}>$</button>
-        <button className={`button ${selectedOptions.includes(2) ? "selected" : ""}`} onClick={() => handleOptionClick(2)}>$$</button>
-        <button className={`button ${selectedOptions.includes(3) ? "selected" : ""}`} onClick={() => handleOptionClick(3)}>$$$</button>
-        <button className={`button ${selectedOptions.includes(4) ? "selected" : ""}`} onClick={() => handleOptionClick(4)}>$$$$</button>
+      <div className="App">
+        <h1>Select Options:</h1>
+        <div className="button-group">
+          <button
+            className={`button ${
+              selectedOptions.includes(1) ? "selected" : ""
+            }`}
+            onClick={() => handleOptionClick(1)}
+          >
+            $
+          </button>
+          <button
+            className={`button ${
+              selectedOptions.includes(2) ? "selected" : ""
+            }`}
+            onClick={() => handleOptionClick(2)}
+          >
+            $$
+          </button>
+          <button
+            className={`button ${
+              selectedOptions.includes(3) ? "selected" : ""
+            }`}
+            onClick={() => handleOptionClick(3)}
+          >
+            $$$
+          </button>
+          <button
+            className={`button ${
+              selectedOptions.includes(4) ? "selected" : ""
+            }`}
+            onClick={() => handleOptionClick(4)}
+          >
+            $$$$
+          </button>
+        </div>
       </div>
-    </div>
-
 
       <div className="side-by-side">
         <ul className="cuisine__list">
