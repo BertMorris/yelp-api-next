8eaa3402e62a1e0dfff081a935e45544c8a0d8e5
sherry122232 <1006370919@qq.com>
2023-05-04 00:45:04 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   postcss.config.js 	modified:   src/pages/[location]/index.tsx 	modified:   src/styles/globals.css

--- src/pages/[location]/index.tsx
+++ src/pages/[location]/index.tsx
@@ -5,8 +5,13 @@ import { useRouter } from "next/router";
 type Props = {};
 
 export default function Cuisines({}: Props) {
+
   const [left, setLeft] = useState<string[]>(["", "", ""]);
   const [right, setRight] = useState<string[]>(["", "", ""]);
+  const [location, setLocation] = useState('');
+  const [selectedRadius, setSelectedRadius] = useState(1);
+  const [selectedOptions, setSelectedOptions] = useState<number[]>([]);
+
 
   const router = useRouter();
   const cuisines = [
@@ -21,6 +26,34 @@ export default function Cuisines({}: Props) {
     "Indian",
   ];
 
+
+  const handleOptionClick = (option:any) => {
+    if (selectedOptions.includes(option)) {
+      setSelectedOptions(selectedOptions.filter((selectedOption) => selectedOption !== option));
+    } else {
+      setSelectedOptions([...selectedOptions, option]);
+    }
+
+    console.log(selectedOptions);
+    
+
+    // console.log(containsFour);
+  };
+  
+
+
+
+
+  const handleLocationChange = (event:any) => {
+    setLocation(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleRadiusChange = (event:any) => {
+    setSelectedRadius(parseInt(event.target.value));
+    console.log(event.target.value);
+  };
+  
   function isDisabled() {
     if (left.includes("") || right.includes("")) {
       return true;
@@ -55,6 +88,36 @@ export default function Cuisines({}: Props) {
       >
         Get Restaurants
       </button>
+
+
+      <form>
+      <label htmlFor="radius">Select Radius:</label>
+      <select id="radius" name="radius" value={selectedRadius} onChange={handleRadiusChange}>
+        <option value={1}>Less than 1 mile</option>
+        <option value={10}>Less than 10 miles</option>
+        <option value={25}>Less than 25 miles</option>
+        <option value={50}>Less than 50 miles</option>
+        <option value={100}>Less than 100 miles</option>
+      </select>
+    </form>
+
+      <form>
+      <label htmlFor="location">Enter Location:</label>
+      <input type="text" id="location" name="location" placeholder="e.g. New York City" value={location} onChange={handleLocationChange} />
+    </form>
+
+
+    <div className="App">
+      <h1>Select Options:</h1>
+      <div className="button-group">
+        <button className={`button ${selectedOptions.includes(1) ? "selected" : ""}`} onClick={() => handleOptionClick(1)}>$</button>
+        <button className={`button ${selectedOptions.includes(2) ? "selected" : ""}`} onClick={() => handleOptionClick(2)}>$$</button>
+        <button className={`button ${selectedOptions.includes(3) ? "selected" : ""}`} onClick={() => handleOptionClick(3)}>$$$</button>
+        <button className={`button ${selectedOptions.includes(4) ? "selected" : ""}`} onClick={() => handleOptionClick(4)}>$$$$</button>
+      </div>
+    </div>
+
+
       <div className="side-by-side">
         <ul className="cuisine__list">
           {cuisines.map((item) => (
